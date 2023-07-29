69fe56086681ff88cee7d2ccdf23a34db69300d5
Masaaki Taguchi <88762785+masaaki-taguchi@users.noreply.github.com>
2023-07-29 18:24:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,153 @@
+# proc-web-con
+<p align="center">
+  <img src="https://img.shields.io/badge/JavaScript-yellow.svg?logo=JavaScript&logoColor=white">
+  <img src="https://img.shields.io/badge/NodeJS-339933.svg?logo=Node.js&logoColor=white">
+  <img src="https://img.shields.io/badge/license-MIT-blue.svg">
+</p>
+
+Webブラウザよりプロセスの起動/停止、OSのリブート/シャットダウンを行います。   
+設定ファイルを変更することで実行コマンドの変更や追加、表示文字列やボタンのスタイルの変更が可能です。   
+主にRaspberry Piでの使用を想定していますが、他のOSでも使用可能かと思います。   
+
+[<kbd><img src="./images/procwebcon_en.png" width="200"></kbd>](./images/procwebcon_en.png)[<kbd><img src="./images/procwebcon_ja.png" width="200"></kbd>](./images/procwebcon_ja.png)
+
+## インストール
+### Node.jsのインストール
+本ツールはNode.jsを使用します。32bit版Raspberry Piであれば、以下の手順でインストールします。   
+(環境差分がある部分は適宜変更してください。)
+
+1. https://nodejs.org/ja/download より、ARMv7のリンクを取得
+
+2. /home/piにディレクトリを変更し、1.で取得したリンクを使ってwgetを実行
+```
+wget https://nodejs.org/dist/v18.17.0/node-v18.17.0-linux-armv7l.tar.xz
+```
+3. 2.で取得したファイルを展開
+```
+tar xvf node-v18.17.0-linux-armv7l.tar.xz
+```
+4. 3.で作成されたディレクトリに合わせて.bashrcでパスを追加
+```
+echo 'export PATH=$HOME/node-v18.17.0-linux-armv7l/bin:$PATH' > ~/.bashrc
+source ~/.bashrc
+```
+
+また、後の手順でnpmコマンドによる各ライブラリの取得がありますが、Raspberry Pi上ではリポジトリへのアクセスに失敗する可能性があります。その場合は、/etc/hostsに以下を追加してください。
+```
+104.16.20.35 registry.npmjs.org
+```
+
+### proc-web-conのインストール
+以下のファイルを適当な場所に配置します。
+
+* proc-web-con.js
+* proc-web-con.ejs
+* user_config.yaml
+* user_config_ja.yaml
+* user_config_simple.yaml
+* package.json
+* package-lock.json
+
+以下のコマンドを実行し、必要なライブラリをインストールします。
+```
+npm ci
+```
+### サンプルの設定ファイル
+設定ファイルのサンプルは3種類あります。全て[ras68k-ext](http://opmregisters.web.fc2.com/ras68k/)を対象としたサンプルとなっています。
+| &nbsp;&nbsp;&nbsp;&nbsp;ファイル名&nbsp;&nbsp;&nbsp;&nbsp; | 内容 |
+| ---- | ---- |
+|  user_config.yaml  | 設定可能な全ての値を定義しています。ラベル名は英語にしています。また、user_config.yamlはツールのデフォルト設定ファイル名となります。   |
+|  user_config_ja.yaml  |  user_config.yamlの日本語版です。  |
+|  user_config_simple.yaml  | ツールでデフォルト指定が可能なものを除いたシンプルな設定ファイルです。最低限このファイルの設定値で動作可能です。   |
+
+### 設定ファイルの編集
+設定名から推測できるものは除いて、ポイントのみ説明をします。   
+
+基本設定のtitleはブラウザのタイトルバーと、画面本体のタイトルに使用されます。   
+titleStyleは画面本体のタイトルの表示スタイルの指定です。   
+buttonStyleはプロセス確認の更新ボタン、及びコマンド定義されたボタンのスタイルの指定となります。
+```
+# 基本設定
+title: 'ras68k-ext controller'
+titleStyle: 'dark'
+buttonStyle: 'primary'
+listenPort: 3000
+executingMessage: '実行中...'
+```
+使用可能なスタイルは以下となります。   
+[<img src="./images/style.png" width="200">](./images/style.png)
+
+プロセス監視関連設定のprocessNameは稼働状況を監視したいプロセス名を指定します。grepコマンドに指定するものであるため、厳密にプロセス名と完全一致しなくても問題ありません。例では"pi68k-ext"とありますが、実際には"pi68k-ext_r2"または"pi68k-ext_r3"のプロセスの稼働を確認します。
+
+```
+# プロセス監視関連設定
+processName: 'pi68k-ext'
+processRunningMessage: 'プロセス状況: 起動中'
+processStoppingMessage: 'プロセス状況: 停止中'
+refreshButtonName: '更新'
+```
+
+OS 再起動/シャットダウン関連設定のosRebootShutdownButtonStyleは再起動、シャットダウンボタンのスタイルの指定です。   
+rebootCmd、shutdownCmdで実行するコマンド名が指定可能です。   
+rebootConfirmMessage、shutdownConfirmMessageは実行前に確認するダイアログに表示する文字列です。
+
+```
+# OS 再起動/シャットダウン関連設定
+osRebootShutdownTitle: 'Raspberry Pi 再起動/シャットダウン'
+osRebootShutdownButtonStyle: 'danger'
+rebootCmd: 'sudo reboot'
+rebootButtonName: '再起動'
+rebootConfirmMessage: '再起動しますか？'
+shutdownCmd: 'sudo shutdown -h now'
+shutdownButtonName: 'シャットダウン'
+shutdownConfirmMessage: 'シャットダウンしますか？'
+```
+
+コマンド定義ではcommands配下に実行したいコマンド内容とボタン名を複数指定可能です。   
+nameがボタン名、cmdが実行コマンド、dirがコマンド実行ディレクトリ、timeoutはタイムアウト時間(ms)の指定です。
+
+```
+# コマンド定義
+commandsTitle: 'ras68k-ext 起動/停止'
+commands:
+  - name: '起動'
+    cmd: 'sudo /home/pi/pcmd/pi68k.py start'
+    dir: '/home/pi/pcmd'
+    timeout: 5000
+  - name: '停止'
+    cmd: 'sudo /home/pi/pcmd/pi68k.py stop'
+    dir: '/home/pi/pcmd'
+    timeout: 30000
+```
+
+## 使用方法
+以下のコマンドで起動可能です。オプション指定がない場合は、設定ファイルはuser_config.yamlが使用されます。
+```
+node proc-web-con.js
+```
+実行後、以下が表示されればブラウザからアクセス可能です。   
+Raspberry Piに割り当てられたIPアドレス、またはlocalhostに対してポート番号を付与してアクセスしてください。
+```
+Server started. ListenPort: 3000
+```
+
+設定ファイルを指定したい場合は-cオプションを使用します。
+```
+node proc-web-con.js -c user_config_ja.yaml
+```
+
+## 自動起動設定
+OS起動時にproc-web-conを自動起動するようにします。
+
+.bashrcを編集します。
+```
+vi ~/.bashrc
+```
+設定ファイル指定ありで起動し、且つnohup.logにログは出力しない場合は、以下のような指定をして、保存します。
+```
+nohup node /home/pi/proc-web-con/proc-web-con.js -c user_config_ja.yaml >/dev/null 2>&1 &
+```
+
+## ライセンス
+proc-web-conはMITライセンスを適用しています。
+
---
+++ proc-web-con.js
@@ -0,0 +1,301 @@
+'use strict';
+
+const http = require("http");
+const express = require("express");
+const execSync = require('child_process').execSync;
+
+const COMMAND_OPTION_VERBOSE_MODE = '-v';
+const COMMAND_OPTION_HELP = '-h';
+const COMMAND_OPTION_CONFIG = '-c';
+
+const DEFAULT_USER_CONFIG_PATH = './user_config.yaml';
+const DEFAULT_EJS_NAME = 'proc-web-con';
+const DEFAULT_TITLE_STYLE = 'dark';
+const DEFAULT_BUTTON_STYLE = 'primary';
+const DEFAULT_LISTEN_PORT = 3000;
+const DEFAULT_TIMEOUT = 30000;
+const DEFAULT_EXECUTING_MESSAGE = 'Executing...';
+const DEFAULT_PROCESS_RUNNING_MESSAGE = 'ProcessStatus: Running';
+const DEFAULT_PROCESS_STOPPING_MESSAGE = 'ProcessStatus: Stopping';
+const DEFAULT_OS_REBOOT_SHUTDOWN_TITLE = 'OS Reboot/Shutdown';
+const DEFAULT_OS_REBOOT_SHUTDOWN_BUTTON_STYLE = 'danger';
+const DEFAULT_REFRESH_BUTTON_NAME = 'Refresh';
+const DEFAULT_REBOOT_BUTTON_NAME = 'Reboot';
+const DEFAULT_REBOOT_CONFIRM_MESSAGE = 'Reboot OK?';
+const DEFAULT_REBOOT_CMD = 'sudo reboot';
+const DEFAULT_SHUTDOWN_BUTTON_NAME = 'Shutdown';
+const DEFAULT_SHUTDOWN_CONFIRM_MESSAGE = 'Shutdown OK?';
+const DEFAULT_SHUTDOWN_CMD = 'sudo shutdown -h now';
+
+const WAIT_TIME_BEFORE_REFRESH = 1000;
+
+// default config path
+let userConfigPath = DEFAULT_USER_CONFIG_PATH;
+
+// analyzes command line options
+global.enabledLogging = false;
+let paramList = [];
+let paramCnt = 0;
+for (let i = 2; i < process.argv.length; i++) {
+  if (process.argv[i] === COMMAND_OPTION_VERBOSE_MODE) {
+    global.enabledLogging = true;
+  } else if (process.argv[i] === COMMAND_OPTION_CONFIG) {
+    if (i + 1 >= process.argv.length) {
+      usage();
+    }
+    userConfigPath = process.argv[i + 1];
+  } else if (process.argv[i] === COMMAND_OPTION_HELP) {
+    usage();
+  } else {
+    paramList.push(process.argv[i]);
+  }
+}
+
+// default parameter
+loadUserConfig(userConfigPath);
+let userConfig = global.userConfig;
+
+let ejsName = DEFAULT_EJS_NAME;
+if (userConfig.ejsName) {
+  ejsName = userConfig.ejsName;
+}
+
+let titleStyle = DEFAULT_TITLE_STYLE;
+if (userConfig.titleStyle) {
+  titleStyle = userConfig.titleStyle;
+}
+
+let buttonStyle = DEFAULT_BUTTON_STYLE;
+if (userConfig.buttonStyle) {
+  buttonStyle = userConfig.buttonStyle;
+}
+
+let listenPort = DEFAULT_LISTEN_PORT;
+if (userConfig.listenPort && !isNaN(userConfig.listenPort)) {
+  listenPort = userConfig.listenPort;
+}
+
+let executingMessage = DEFAULT_EXECUTING_MESSAGE;
+if (userConfig.executingMessage) {
+  executingMessage = userConfig.executingMessage;
+}
+
+let refreshButtonName = DEFAULT_REFRESH_BUTTON_NAME;
+if (userConfig.refreshButtonName) {
+  refreshButtonName = userConfig.refreshButtonName;
+}
+let processStatusMessageList = []
+if (userConfig.processRunningMessage && userConfig.processStoppingMessage) {
+  processStatusMessageList.push(userConfig.processRunningMessage);
+  processStatusMessageList.push(userConfig.processStoppingMessage);
+} else {
+  processStatusMessageList.push(DEFAULT_PROCESS_RUNNING_MESSAGE);
+  processStatusMessageList.push(DEFAULT_PROCESS_STOPPING_MESSAGE);
+}
+
+let osRebootShutdownTitle = DEFAULT_OS_REBOOT_SHUTDOWN_TITLE;
+if (userConfig.osRebootShutdownTitle) {
+  osRebootShutdownTitle = userConfig.osRebootShutdownTitle;
+}
+let osRebootShutdownButtonStyle = DEFAULT_OS_REBOOT_SHUTDOWN_BUTTON_STYLE;
+if (userConfig.osRebootShutdownButtonStyle) {
+  osRebootShutdownButtonStyle = userConfig.osRebootShutdownButtonStyle;
+}
+let rebootButtonName = DEFAULT_REBOOT_BUTTON_NAME;
+if (userConfig.rebootButtonName) {
+  rebootButtonName = userConfig.rebootButtonName;
+}
+let rebootConfirmMessage = DEFAULT_REBOOT_CONFIRM_MESSAGE;
+if (userConfig.rebootConfirmMessage) {
+  rebootConfirmMessage = userConfig.rebootConfirmMessage;
+}
+let rebootCmd = DEFAULT_REBOOT_CMD;
+if (userConfig.rebootCmd) {
+  rebootCmd = userConfig.rebootCmd;
+}
+let shutdownButtonName = DEFAULT_SHUTDOWN_BUTTON_NAME;
+if (userConfig.shutdownButtonName) {
+  shutdownButtonName = userConfig.shutdownButtonName;
+}
+let shutdownConfirmMessage = DEFAULT_SHUTDOWN_CONFIRM_MESSAGE;
+if (userConfig.shutdownConfirmMessage) {
+  shutdownConfirmMessage = userConfig.shutdownConfirmMessage;
+}
+let shutdownCmd = DEFAULT_SHUTDOWN_CMD;
+if (userConfig.shutdownCmd) {
+  shutdownCmd = userConfig.shutdownCmd;
+}
+
+let itemMap = new Map();
+itemMap.set('title', userConfig.title);
+itemMap.set('titleStyle', titleStyle);
+itemMap.set('buttonStyle', buttonStyle);
+itemMap.set('executingMessage', executingMessage);
+itemMap.set('executingFlag', false);
+itemMap.set('osRebootShutdownTitle', osRebootShutdownTitle);
+itemMap.set('osRebootShutdownButtonStyle', osRebootShutdownButtonStyle);
+itemMap.set('refreshButtonName', refreshButtonName);
+itemMap.set('rebootButtonName', rebootButtonName);
+itemMap.set('rebootConfirmMessage', rebootConfirmMessage);
+itemMap.set('shutdownButtonName', shutdownButtonName);
+itemMap.set('shutdownConfirmMessage', shutdownConfirmMessage);
+itemMap.set('commandsTitle', userConfig.commandsTitle);
+let commandButtonList = [];
+for (let command of global.commandList) {
+  commandButtonList.push(command.name); 
+}
+itemMap.set('commandButtons', commandButtonList);
+
+(async () => {
+  let app = express();
+  app.set("view engine", "ejs");
+  app.set("views", __dirname);
+
+  app.get("/", async function (req, res) {
+    checkProcess(itemMap, processStatusMessageList);
+    return res.render(ejsName, Object.fromEntries(itemMap));
+  });
+
+  app.get("/command", async function (req, res) {
+    let commandNum = Number(req.query.command_num);
+    let command = global.commandList[commandNum];
+    let cmd = command.cmd;
+    let dir = command.dir;
+    let timeout = command.timeout;
+    if (!timeout) {
+      timeout = DEFAULT_TIMEOUT;
+    }
+    logging('cmd: ' + cmd + ', dir: ' + dir + ', timeout: ' + timeout);
+    try {
+      var result = '';
+      if (dir) {
+        result = execSync(cmd, {cwd: dir, timeout: timeout}).toString(); 
+      } else {
+        result = execSync(cmd, {timeout: timeout}).toString(); 
+      }
+      logging('result: ' + result);
+    } catch (error) {
+      logging('error occured. cmd: ' + cmd);
+      logging(error);
+    }
+    await sleep(WAIT_TIME_BEFORE_REFRESH);
+    checkProcess(itemMap, processStatusMessageList);
+    return res.render(ejsName, Object.fromEntries(itemMap));
+  });
+
+  app.get("/refresh", async function (req, res) {
+    checkProcess(itemMap, processStatusMessageList);
+    return res.render(ejsName, Object.fromEntries(itemMap));
+  });
+
+  app.get("/reboot", async function (req, res) {
+    try {
+      logging('reboot cmd: ' + rebootCmd);
+      res.redirect('/');
+      var result = '';
+      result = execSync(rebootCmd).toString(); 
+    } catch (error) {
+      logging(error);
+    }
+  });
+
+  app.get("/shutdown", async function (req, res) {
+    try {
+      logging('shutdown cmd: ' + shutdownCmd);
+      res.redirect('/');
+      var result = '';
+      result = execSync(shutdownCmd).toString(); 
+    } catch (error) {
+      logging(error);
+    }
+  });
+
+  var server = http.createServer(app);
+  server.listen(listenPort);
+  console.log('Server started. ListenPort: ' + listenPort);
+
+})();
+
+function sleep(waitTime) {
+  return new Promise(function (resolve) {
+    setTimeout(function() { resolve() }, waitTime);
+  });
+} 
+
+function checkProcess(itemMap, processStatusMessageList) {
+  try {
+    let processName = userConfig.processName;
+    const result = execSync('ps -ef | grep "' + processName + '" | grep -v grep'); 
+    logging(result.toString());
+    itemMap.set('processStatusMessage', processStatusMessageList[0]);
+  } catch (error) {
+    itemMap.set('processStatusMessage', processStatusMessageList[1]);
+  }
+}
+
+function loadYamlFile(fileName) {
+  const fs = require('fs');
+  let existsFile = fs.existsSync(fileName);
+  if (!existsFile) {
+    console.error('File not found. filePath: ' + fileName);
+    process.exit(1);
+  }
+  const yaml = require('js-yaml');
+  const yamlText = fs.readFileSync(fileName, 'utf8');
+  return yaml.load(yamlText);
+}
+
+function loadUserConfig(userConfigPath) {
+  let userConfigPathWork = userConfigPath;
+  if (userConfigPathWork === undefined) {
+    userConfigPathWork = DEFAULT_USER_CONFIG_PATH;
+  }
+  const path = require('path');
+  const config = loadYamlFile(path.join(__dirname, userConfigPathWork));
+  global.userConfig = config;
+
+  let commandList = [];
+  for (const command of config.commands) {
+    commandList.push(new _command(
+      command.name,
+      command.cmd,
+      command.dir,
+      command.timeout
+    ));
+  }
+  global.commandList = commandList;
+}
+
+function _command(name, cmd, dir, timeout) {
+  this.name = name;
+  this.cmd = cmd;
+  this.dir = dir;
+  this.timeout = timeout;
+}
+
+function logging(message) {
+  if (global.enabledLogging) {
+    const nowDate = new Date();
+    console.log('[' + getFormattedDateTime(nowDate) + '] ' + message);
+  }
+}
+
+function getFormattedDateTime(date) {
+  let dateString =
+    date.getFullYear() + '/' +
+    ('0' + (date.getMonth() + 1)).slice(-2) + '/' +
+    ('0' + date.getDate()).slice(-2) + ' ' +
+    ('0' + date.getHours()).slice(-2) + ':' +
+    ('0' + date.getMinutes()).slice(-2) + ':' +
+    ('0' + date.getSeconds()).slice(-2);
+  return dateString;
+}
+
+function usage() {
+  console.log('usage: proc-web-con.js [-options]');
+  console.log('    -c <pathname> specifies a config file path (default is ./user_config.yaml)');
+  console.log('    -v            verbose mode');
+  console.log('    -h            display this help');
+  process.exit(0);
+}
+
